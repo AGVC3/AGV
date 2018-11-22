a6e1284b71cadec32df1bf12c0818aaa3898136d
hanno00 <brandwijkh@gmail.com>
2018-11-22 13:20:25 +0100

    Switches

--- agv/src/RemoteControl.java
+++ agv/src/RemoteControl.java
@@ -22,95 +22,64 @@ public class RemoteControl implements Updateble {
                 this.control.turnWhileDriving(1);
                 break;
             case 4:
-                this.control.turn(-5);
+                this.control.turn(-10);
                 break;
             case 5:
                 this.control.emegencyBreak();
                 break;
             case 6:
-                this.control.turn(5);
+                this.control.turn(10);
                 break;
             case 7:
-                this.control.turn(45);
-                BoeBot.wait(10);
-                this.control.goToSpeed(speedBackwards);
+                this.control.turnWhileDriving(-1);
                 break;
             case 8:
                 this.control.goToSpeed(speedBackwards);
                 break;
             case 9:
-                this.control.turn(-45);
-                BoeBot.wait(10);
-                this.control.goToSpeed(speedBackwards);
+                this.control.turnWhileDriving(1);
                 break;
         }
-//        if (button == 1) {                                      //turn -45 degrees and drive forwards
-//            this.control.turn(-45);
-//            BoeBot.wait(10);
-//            this.control.goToSpeed(defaultSpeed);
-//        }
-//        if (button == 2) {                                      //drive forwards
-//            this.control.goToSpeed(defaultSpeed);
-//        }
-//        if (button == 3) {                                      //turn 45 degrees and drive forwards
-//            this.control.turn(45);
-//            BoeBot.wait(10);
-//            this.control.goToSpeed(defaultSpeed);
-//        }
-//        if (button == 4) {                                      //turn to the left
-//            this.control.turn(-5);
-//        }
-//        if (button == 5) {                                      //stop
-//            this.control.emegencyBreak();
-//        }
-//        if (button == 6) {                                      //turn to the right
-//            this.control.turn(5);
-//        }
-//        if (button == 7) {                                      //turn 45 degrees and drive backwards
-//            this.control.turn(45);
-//            BoeBot.wait(10);
-//            this.control.goToSpeed(speedBackwards);
-//        }
-//        if (button == 8) {                                      //drive backwards
-//            this.control.goToSpeed(speedBackwards);
-//        }
-//        if (button == 9) {                                      //turn -45 degrees and drive backwards
-//            this.control.turn(-45);
-//            BoeBot.wait(10);
-//            this.control.goToSpeed(speedBackwards);
-//        }
     }
 
     private int binaryToButton(int ioPort) {
         String button = buttonToBinary(ioPort);
-        if (button.equals("000010000000")) {
-            return 1;
-        }
-        if (button.equals("000010000001")) {
-            return 2;
-        }
-        if (button.equals("000010000010")) {
-            return 3;
-        }
-        if (button.equals("000010000011")) {
-            return 4;
-        }
-        if (button.equals("000010000100")) {
-            return 5;
-        }
-        if (button.equals("000010000101")) {
-            return 6;
-        }
-        if (button.equals("000010000110")) {
-            return 7;
-        }
-        if (button.equals("000010000111")) {
-            return 8;
-        }
-        if (button.equals("000010001000")) {
-            return 9;
-        } else {
-            return -1;
+
+        switch (button) {
+            case "000010001001":
+                return 0;
+            case "000010000000":
+                return 1;
+            case "000010000001":
+                return 2;
+            case "000010000010":
+                return 3;
+            case "000010000011":
+                return 4;
+            case "000010000100":
+                return 5;
+            case "000010000101":
+                return 6;
+            case "000010000110":
+                return 7;
+            case "000010000111":
+                return 8;
+            case "000010001000":
+                return 9;
+            case "000010010101":
+                return 10;                                     //Power
+            case "000010010000":
+                return 11;                                     //UP
+            case "000010010001":
+                return 12;                                     //DOWN
+            case "000010010011":
+                return 13;                                     //LEFT
+            case "000010010010":
+                return 14;                                     //RIGHT
+            case "000010010100":
+                return 15;                                     //MUTE
+            default:
+                return -1;
         }
     }
 
